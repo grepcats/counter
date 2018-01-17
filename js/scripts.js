635abcc4180c8147ecfd82800dac18968fcacdff
Cameron Anderson and Kayla Ondracek <cameron@>
2018-01-17 08:34:01 -0800

    add error handling

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,24 @@ $(document).ready(function() {
   var countTo = parseInt(prompt("What number do you want to count to?"));
   var multiple = parseInt(prompt("What number do you want to count by?"));
 
+  if (isNaN(countTo) || isNaN(multiple)) {
+    alert("Thats not a number");
+    return;
+  }
+
+  if (countTo <= 0 || multiple <= 0) {
+    alert("Too small");
+    return;
+  }
+
+  if (multiple > countTo) {
+    alert("multiple is too big");
+    return;
+  }
+
   $(".output").append("Count to: " + countTo + "<br>");
   $(".output").append("Count by: " + multiple + "<br>");
   $(".output").append("Output: ");
-
 
   for (var index = multiple; index <= countTo; index += multiple) {
     if ((countTo - index) < multiple) {
@@ -13,9 +27,5 @@ $(document).ready(function() {
     } else {
       $(".output").append(index + ", ");
     }
-
-
   }
-
-
 });
